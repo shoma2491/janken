5727130fc3c02a03dd42145b96f54433dd35216f
shoma2491 <e1b19062@st.oit.ac.jp>
2021-11-08 22:54:14 +0900

    じゃんけんを実装

--- src/main/java/oit/is/z0031/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0031/kaizi/janken/controller/Lec02Controller.java
@@ -2,14 +2,34 @@ package oit.is.z0031.kaizi.janken.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Lec02Controller {
+  String player;
+  String result;
+
   @PostMapping("lec02")
   public String lec02(@RequestParam String name1, ModelMap model) {
     model.addAttribute("name1", name1);
     return "lec02.html";
   }
+
+  @GetMapping("lec02/{te}")
+  public String gu(@PathVariable String te, ModelMap model) {
+    model.addAttribute("player", te);
+    if (te.equals("gu")) {
+      result = "Draw!";
+    } else if (te.equals("choki")) {
+      result = "You Lose!";
+    } else {
+      result = "You Win!";
+    }
+    model.addAttribute("result", result);
+    return "lec02.html";
+  }
+
 }
--- src/main/java/oit/is/z0031/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z0031/kaizi/janken/model/Janken.java
@@ -1,34 +1,15 @@
 package oit.is.z0031.kaizi.janken.model;
 
-import java.util.ArrayList;
-
 public class Janken {
-  ArrayList<Double> numList = new ArrayList<>();
-  double ave;
+  String te;
 
   // コンストラクタ
-  public Janken(ArrayList<Double> numList) {
-    this.numList = numList;
-    double sum = 0;
-    for (Double num : numList) {
-      sum = sum + num;
-    }
-    ave = sum / numList.size();
+  public Janken(String te) {
+    this.te = te;
   }
 
-  public ArrayList<Double> getNumList() {
-    return numList;
+  public String gette() {
+    return this.te;
   }
 
-  public void setNumList(ArrayList<Double> numList) {
-    this.numList = numList;
-  }
-
-  public double getAve() {
-    return ave;
-  }
-
-  public void setAve(double ave) {
-    this.ave = ave;
-  }
 }
